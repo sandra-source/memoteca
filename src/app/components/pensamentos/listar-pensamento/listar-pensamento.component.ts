6744cc465441f2fbcd9d38965e6165699cbb7c04
sandra <contato.sandracarvalho@gmail.com>
2023-07-05 21:44:37 -0300

    Iniciando integração com backend fake utilizando json-server apis.

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com o componente @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: '4EEVJlhaHAuXehkZAKZ2h2Cdghr63Wvm2AzkjTrgnZhfhtp4g8L24DauHsj49ypLwXMcdsadsa dsadasdas dsadsadsadas dasdaWWvV5uqSAfb6xFbcQz0T3mdlaTlSdzEKhVP8Rm9EALQLFMfmJwmkNaGI3VyHHafps8dxNygReFlN7JvwQAOXFu6HQzlfS1gZJyyVbNiZ8oX3EoQjiEG86jPTZZsKqquBYErikgpeUNV190jBV4hRBXJper89zNYtwCe27Z7QMIMASH4gp3dTd9',
-      autoria: 'Componente filho',
-      modelo: 'modelo1'
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
