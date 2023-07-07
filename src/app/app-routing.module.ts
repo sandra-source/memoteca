b6ff19be63ee5324387dc8787058a78f1cc0c099
sandra <contato.sandracarvalho@gmail.com>
2023-07-07 00:30:52 -0300

    Crud finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {
@@ -16,7 +18,16 @@ const routes: Routes = [
   {
     path: 'listar-pensamento',
     component: ListarPensamentoComponent
-  }
+  },
+  {
+    path: 'pensamentos/excluir-pensamento/:id',
+    component: ExcluirPensamentoComponent
+  },
+  {
+    path: 'pensamentos/editar-pensamento/:id',
+    component: EditarPensamentoComponent
+  },
+
 ];
 
 @NgModule({
